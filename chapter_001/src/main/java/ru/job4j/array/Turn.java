ff1929fe553f5c12235916f292e023edaf0e358c
Karen Shakhnazaryan <shakasuone@gmail.com>
2019-11-07 00:52:12 +0300

    Перевернуть массив.[#194876]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class Turn {
+    public int[] back(int[] array) {
+        // toThisIndex - середина массива
+        double toThisIndex = (int) ((array.length/2)-1);
+        int tmp;
+        for (int index = 0; index<=toThisIndex; index++) {
+            tmp = array[index];
+            array[index] = array[array.length - 1 - index];
+            array[array.length - 1 - index] = tmp;
+        }
+        return array;
+    }
+}
