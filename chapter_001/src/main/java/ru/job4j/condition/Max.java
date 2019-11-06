9b3bb3aa96d01db986a9165c61a6568b4f52dbf8
Karen Shakhnazaryan <shakasuone@gmail.com>
2019-11-06 14:13:36 +0300

    Максимум из двух чисел[#194847] Понял, что строгое неравество ничего не ломает

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -2,8 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = (left>=right) ? left : right; //нестрогое сравнение позволит
-        // избежать ошибки при равенстве параметров данного метода
+        int result = (left>right) ? left : right;
         return result;
     }
 }
