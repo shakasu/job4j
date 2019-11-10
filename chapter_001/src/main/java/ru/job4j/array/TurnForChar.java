c4e2db12ee23b295f38226c95d6284b7625889dc
Karen Shakhnazaryan <shakasuone@gmail.com>
2019-11-11 02:29:53 +0300

    Простое число.[#194863]

--- chapter_001/src/main/java/ru/job4j/array/TurnForChar.java
+++ chapter_001/src/main/java/ru/job4j/array/TurnForChar.java
@@ -2,10 +2,8 @@ package ru.job4j.array;
 
 public class TurnForChar {
     public char[] back(char[] array) {
-        // toThisIndex - середина массива
-        double toThisIndex = (int) ((array.length/2)-1);
         char tmp;
-        for (int index = 0; index<=toThisIndex; index++) {
+        for (int index = 0; index < (array.length / 2); index++) {
             tmp = array[index];
             array[index] = array[array.length - 1 - index];
             array[array.length - 1 - index] = tmp;
