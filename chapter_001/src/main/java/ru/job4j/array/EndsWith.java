0f72c9a2fb41b44e639498532b26dec26e205956
Karen Shakhnazaryan <shakasuone@gmail.com>
2019-11-13 15:28:59 +0300

    Слово заканчивается с ...[#194877]

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -3,27 +3,16 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = false;
+        int indexDifference = word.length - post.length - 1;
+        int indexPost = post.length - 1;
         int counter = 0;
-        int i = word.length - 1;
-        int n = post.length - 1;
-        while (i != n) {
-            i--;
-        }
-        i = word.length - i - 1;
-        n = 0;
-        while (i != word.length) {
-            if (word[i] == post[n]) {
+        for (int indexWord = word.length - 1; indexWord > indexDifference; indexWord--) {
+            if (word[indexWord] == post[indexPost]) {
                 counter++;
+                indexPost--;
             }
-            i++;
-            n++;
         }
         result = (counter == post.length);
-        /*TurnForChar turner = new TurnForChar();
-        char[] returnPost = turner.back(post);
-        char[] returnWord = turner.back(word);
-        ArrayChar checker = new ArrayChar();
-        boolean result = ArrayChar.startsWith(word, post);*/
         return result;
     }
 }
---
+++ chapter_001/src/main/java/ru/job4j/loop/Test.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class Test {
+    public static void main(String[] args) {
+
+        char[] list = new char[]  {'d', 'f', 'd', 'f'};
+
+        for (char i:list) {
+            System.out.println(i);
+        }
+    }
+}
