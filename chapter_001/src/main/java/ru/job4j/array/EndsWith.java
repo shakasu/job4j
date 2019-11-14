4512f34bee6a9d2c7dd9df170b168599b2d35ce5
Karen Shakhnazaryan <shakasuone@gmail.com>
2019-11-14 22:46:07 +0300

    Слово заканчивается с ...[#194877]

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -1,16 +1,27 @@
 package ru.job4j.array;
 
+/**
+ * A class for checking char-type "word" arrays for the end of a "post".
+ *
+ * @author Karen Shakhnazaryan (shakasuone@gmail.com)
+ *
+ */
 public class EndsWith {
+    /**
+     *
+     * @param word - verifiable array.
+     * @param post - checking array.
+     * @return - truth of check.
+     */
     public static boolean endsWith(char[] word, char[] post) {
         int indexDifference = word.length - post.length - 1;
         int indexPost = post.length - 1;
-        int counter = 0;
         for (int indexWord = word.length - 1; indexWord > indexDifference; indexWord--) {
             if (word[indexWord] != post[indexPost]) {
-                return false;
+                break;
             }
-                indexPost--;
+            indexPost--;
         }
-        return true;
+        return indexPost == -1 ? true : false;
     }
 }
