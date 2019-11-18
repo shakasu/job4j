b238d0c917775f9e9330e39420c29ccf634564f3
Karen Shakhnazaryan <shakasuone@gmail.com>
2019-11-18 17:04:04 +0300

    Рефакторинг - Расстояние между точками.[#194990]

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -7,12 +7,10 @@ public class PointTest {
 
     @Test
     public void distance() {
-        int in1 = -12;
-        int in2 = -35;
-        int in3 = 72;
-        int in4 = 210;
+        Point a = new Point(-12, -35);
+        Point b = new Point(72, 210);
+        double out = a.distance(b);
         double expected = 259;
-        double out = Point.distance(in1, in2, in3, in4);
         Assert.assertEquals(expected, out, 0.01);
 
     }
