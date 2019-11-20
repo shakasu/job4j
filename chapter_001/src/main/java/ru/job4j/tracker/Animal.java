0f38725f7f7982bb8236281a1a6c037c94817779
Karen Shakhnazaryan <shakasuone@gmail.com>
2019-11-20 17:15:01 +0300

    Конструктор по умолчанию.[#195007]

---
+++ chapter_001/src/main/java/ru/job4j/tracker/Animal.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+public class Animal {
+
+    public Animal() {
+        super();
+        System.out.println("load animal");
+    }
+
+    public static void main(String[] args) {
+        Animal tiger = new Tiger();
+    }
+
+}
