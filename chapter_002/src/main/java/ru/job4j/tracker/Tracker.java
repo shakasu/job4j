659e6b191f65f971cd4d4f1d78fcd3e90de75dc0
Karen Shakhnazaryan <shakasuone@gmail.com>
2019-11-23 01:17:37 +0300

    Реализовать класс Tracker[#195012]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,19 +1,16 @@
 package ru.job4j.tracker;
 
-public class Tracker {
-    /**
-     * Массив для хранение заявок.
-     */
-    private final Item[] items = new Item[100];
+import java.util.Random;
 
-    /**
-     * Указатель ячейки для новой заявки.
-     */
+public class Tracker {
+    private final Item[] items = new Item[100];
     private int position = 0;
 
     /**
-     * Метод реализаущий добавление заявки в хранилище
-     * @param item новая заявка
+     * Метод add присваивает ячейке массива items новое значение.
+     * Происхоит генерация уникального id и сдвигается счетчик position.
+     * @param item
+     * @return
      */
     public Item add(Item item) {
         item.setId(this.generateId());
@@ -22,12 +19,123 @@ public class Tracker {
     }
 
     /**
-     * Метод генерирует уникальный ключ для заявки.
-     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
-     * @return Уникальный ключ.
+     * Метод replace проверяет возможность замены ячейки массива items.
+     * А так же заменяет ее на новый элемент типа Item.
+     * False возможен при неподходящем id.
+     * 38 строчка присваивает старой ячейке имя новой.
+     * 39 строчка почему-то не может присвоить имя.
+     * Почему 39 строчка не работает?
+     * @param id
+     * @param newItem
+     * @return
+     */
+    public boolean replace(String id, Item newItem) {
+        boolean result = false;
+        for (Item item : items) {
+            if (item != null && item.getId().equals(id)) {
+                result = true;
+                newItem.setId(item.getId());
+                item.setName(newItem.getName());
+                //item = newItem;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     *Метод находит по Id выбранную пользователем ячейку.
+     *Считает ее положение в массиве items.
+     *Удаляет ячейку и сдвигает null в конец массива.
+     *False возможен при неподходящем id.
+     * @param id
+     * @return
+     */
+    public  boolean delete(String id) {
+        boolean result = false;
+        int delIndex = 0;
+        for (Item item : items) {
+            delIndex++;
+            if (item != null && item.getId().equals(id)) {
+                result = true;
+                break;
+            }
+        }
+        if (result) {
+            items[delIndex - 1] = null;
+            for (int i = 0; i < items.length - 1; i++) {
+                items[i] = items[i + 1];
+            }
+            items[items.length - 1] = null;
+        }
+        return result;
+    }
+
+    /**
+     * Находит массив ячеек с ненулевыми ссылками.
+     * @return
+     */
+    public  Item[] findAll() {
+        int indexNotNull = 0;
+        int i = 0;
+        for (Item item : items)  {
+            if (item != null) {
+                indexNotNull++;
+            }
+        }
+        Item[] result = new Item[indexNotNull];
+        for (Item item : items) {
+            if (item != null) {
+                result[i++] = item;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Находит массив ячеек с одинаковым именем
+     * @param key
+     * @return
+     */
+    public  Item[] findByName(String key) {
+        int indexKey = 0;
+        int i = 0;
+        for (Item item : items) {
+            if (item != null && item.getName().equals(key)) {
+                indexKey++;
+            }
+        }
+        Item[] result = new Item[indexKey];
+        for (Item item : items) {
+            if (item != null && item.getName().equals(key)) {
+                result[i++] = item;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Находит элемент массива items, соответствующий ункальному id.
+     * @param id
+     * @return
+     */
+    public  Item findById(String id) {
+        Item result = null;
+        for (Item item : items) {
+            if (item != null && item.getId().equals(id)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Генерирует уникальное число через сумму случайного числа и текущей даты.
+     * @return
      */
     private String generateId() {
-        //Реализовать метод генерации.
-        return null;
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
     }
 }
--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,4 +1,89 @@
 package ru.job4j.tracker;
 
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class TrackerTest {
+    @Test
+    public void add() {
+        //данный метод так же тестирует generateId и findById
+        Tracker tracker = new Tracker();
+        //создаем экземпляр Item
+        Item item = new Item("test1");
+        //добавляем его в массив items
+        tracker.add(item);
+        //ищем в массиве сгенерированный id и ячейку с именем экземплярв
+        Item result = tracker.findById(item.getId());
+        // сравниваем их
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void findByName() {
+        Tracker tracker = new Tracker();
+        // так как метод возвращает массив, то и мы создадим два экземпляра с одинаковыми именами
+        Item test1 = new Item("test123");
+        tracker.add(test1);
+        // а так же один с отличающимся именем
+        Item test2 = new Item("test2");
+        tracker.add(test2);
+        Item test3 = new Item("test123");
+        tracker.add(test3);
+        //ожидаем массив из экземпляров с одинаковыми именами
+        Item[] expected = {test1, test3};
+        //генерируем массив проверяемым методом
+        Item[] result = tracker.findByName(test1.getName());
+        //сравниваем их
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void findAll() {
+        Tracker tracker = new Tracker();
+        //тк метод вернет массив не нулевых элементов, добавим их произвольно
+        Item test1 = new Item("test123");
+        tracker.add(test1);
+        Item test2 = new Item("test2");
+        tracker.add(test2);
+        Item test3 = new Item("test123");
+        tracker.add(test3);
+        //ожидаем такой массив
+        Item[] expected = {test1, test2, test3};
+        //генерируем
+        Item[] result = tracker.findAll();
+        //сравниваем
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void replace() {
+        Tracker tracker = new Tracker();
+        Item previous = new Item("test1");
+        tracker.add(previous);
+        Item next = new Item("test2");
+        // заменяем старый элемент на недобавленный
+        tracker.replace(previous.getId(), next);
+        // проверяем, что элемент с данным именем и id существует в массиве
+        assertThat(tracker.findById(next.getId()).getName(), is("test2"));
+    }
+
+    @Test
+    public void delete() {
+        Tracker tracker = new Tracker();
+        Item test1 = new Item("test1");
+        tracker.add(test1);
+        Item test2 = new Item("test2");
+        tracker.add(test2);
+        Item test3 = new Item("test3");
+        tracker.add(test3);
+        tracker.delete(test2.getId());
+        boolean result = tracker.delete(test2.getId());
+        //так как мы ожидаем логическую переменную и уменьшения количества
+        // ненулевых элементов, то сравним еще длину массива от метода findAll
+        boolean expected = tracker.findAll().length == 2;
+        assertThat(result, is(expected));
+    }
 }
