c7c2919a3cd123197889b52ebc9f6e1436fa9ac3
Karen Shakhnazaryan <shakasuone@gmail.com>
2019-12-12 17:08:23 +0300

    Телефонный справочник на базе ArrayList[#194938]

---
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Karen", "1", "123456", "SPb")
+        );
+        List<Person> persons = phones.find("Karen");
+        assertThat(persons.iterator().next().getSurname(), is("1"));
+    }
+}
