3ab823a182023a103a95a5fc2328454c90353098
Karen Shakhnazaryan <shakasuone@gmail.com>
2020-02-15 14:12:25 +0300

    Отсортировать департаменты [#34352]

--- chapter_003/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_003/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -2,44 +2,31 @@ package ru.job4j.collection;
 
 import java.util.Comparator;
 
-/**
- * Метод compare сравнивает левую и правую строчку.
- * по заданию: первые элементы по убываюнию, последующие по возрастанию.
- * вводим результат сравнение элементов compareInt.
- * первый оператор условия (строчка 27) сравнит первые элементы двух строк по убыванию,
- * поэтому вернем со знаком "минус" (строчка 30).
- * второй оператор условия (строчка 33) сравнит элементы между вторым и последним из более короткой строчки,
- * по возрастанию, поэтому знак "плюс" (строчка 34).
- * третий оператор (строчка 38) вне цикла нужен для второго крайнего случая, когда длины у строк разные,
- * и строки идентичны вплоть до последнего элемента более длинной.
- * соответственно более длинная строка будет "больше".
- */
-
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String leftStr, String rightStr) {
         int result = 0;
-        String[] leftStrSplit = leftStr.split("/");
-        String[] rightStrSplit = rightStr.split("/");
-        int shorterSize = Math.min(leftStrSplit.length, rightStrSplit.length);
-        Integer leftStrSplitLength = leftStrSplit.length;
-        Integer rightStrSplitLength = rightStrSplit.length;
-        for (int index = 0; index < shorterSize; index++) {
-            char[] leftChar = leftStrSplit[index].toCharArray();
-            char[] rightChar = rightStrSplit[index].toCharArray();
-            int lastCharIndex = leftChar.length - 1;
-            int compareInt = Character.compare(leftChar[lastCharIndex], rightChar[lastCharIndex]);
-            if (index == 0 && compareInt != 0) {
-                result = -compareInt;
+        int minSize = Math.min(leftStr.length(), rightStr.length());
+        boolean charIsDigit = false;
+        int firstDigit = -1;
+        for (int i = 0; i < minSize; i++) {
+            if (!charIsDigit) {
+                charIsDigit = Character.isDigit(leftStr.charAt(i));
+                firstDigit = i;
+            }
+            boolean charNotEqual = leftStr.charAt(i) != rightStr.charAt(i);
+            int charCompare = Character.compare(leftStr.charAt(i), rightStr.charAt(i));
+            if (charNotEqual && i == firstDigit) {
+                result = -charCompare;
                 break;
             }
-            if (index != 0 && compareInt != 0) {
-                result = compareInt;
+            if (charNotEqual) {
+                result = charCompare;
                 break;
             }
-        }
-        if (result == 0) {
-            result = leftStrSplitLength.compareTo(rightStrSplitLength);
+            if (result == 0) {
+                result = Integer.compare(leftStr.length(), rightStr.length());
+            }
         }
         return result;
     }
--- chapter_003/src/test/java/ru/job4j/collection/DepDescCompTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/DepDescCompTest.java
@@ -3,11 +3,8 @@ package ru.job4j.collection;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.lessThan;
-
 
 public class DepDescCompTest {
-
     @Test
     public void compare() {
         int rsl = new DepDescComp().compare(
