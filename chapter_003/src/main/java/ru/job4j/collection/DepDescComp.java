7504d0a1113a0741770c8f93159c2298cba1ee25
Karen Shakhnazaryan <shakasuone@gmail.com>
2020-02-17 20:44:20 +0300

    Отсортировать департаменты [#34352]

--- chapter_003/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_003/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,26 +7,11 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String leftStr, String rightStr) {
         int result = 0;
         int minSize = Math.min(leftStr.length(), rightStr.length());
-        boolean charIsDigit = false;
-        int firstDigit = -1;
         for (int i = 0; i < minSize; i++) {
-            if (!charIsDigit) {
-                charIsDigit = Character.isDigit(leftStr.charAt(i));
-                firstDigit = i;
-            }
-            boolean charNotEqual = leftStr.charAt(i) != rightStr.charAt(i);
-            int charCompare = Character.compare(leftStr.charAt(i), rightStr.charAt(i));
-            if (charNotEqual && i == firstDigit) {
-                result = -charCompare;
-                break;
-            }
-            if (charNotEqual) {
-                result = charCompare;
-                break;
-            }
-            if (result == 0) {
-                result = Integer.compare(leftStr.length(), rightStr.length());
-            }
+            result = Character.compare(leftStr.charAt(i), rightStr.charAt(i));
+        }
+        if (result == 0) {
+            result = Integer.compare(leftStr.length(), rightStr.length());
         }
         return result;
     }
