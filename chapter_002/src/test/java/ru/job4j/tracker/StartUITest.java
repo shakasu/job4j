cf26cb91a065b1738a2540eeeb7b49ed4cd4270b
Karen Shakhnazaryan <shakasuone@gmail.com>
2020-03-19 22:35:17 +0300

    В классе StartUI заменить вывод в консоль на Consumer.[#194811]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -22,13 +22,8 @@ public class StartUITest {
         }
     };
     private final Tracker tracker = new Tracker();
-    private String menu = "0. Add new item.\r\n\r\n"
-            + "1. Show all items.\r\n\r\n"
-            + "2. Edit item.\r\n\r\n"
-            + "3. Delete item.\r\n\r\n"
-            + "4. Find by Id.\r\n\r\n"
-            + "5. Find by name.\r\n\r\n"
-            + "6. Exit\r\n\r\n";
+    private static final String LN = System.lineSeparator();
+    private String menu = String.format("0. Add new item.%s%s1. Show all items.%s%s2. Edit item.%s%s3. Delete item.%s%s4. Find by Id.%s%s5. Find by name.%s%s6. Exit%s%s", LN, LN, LN, LN, LN, LN, LN, LN, LN, LN, LN, LN, LN, LN);
 
     @Before
     public void loadOutput() {
@@ -53,15 +48,16 @@ public class StartUITest {
         new StartUI(input, tracker, output).init();
         String name = tracker.findAll().get(0).getName();
         String id = tracker.findAll().get(0).getId();
-        assertThat(new String(this.out.toByteArray()), is(menu + "Adding item was successful.\r\nName - " + name + " and Id - " + id + " of new item\r\n\r\n" + menu));
-    }
+        String rsl = String.format("%sAdding item was successful.%sName - %s and Id - %s of new item%s%s%s", menu, LN, name, id, LN, LN, menu);
+        assertThat(new String(this.out.toByteArray()), is(rsl));
+}
 
     @Test
     public void showAll() {
         Item item1 = tracker.add(new Item("test1", 12));
         Input input = new StubInput(new String[] {"1", "6"});
         new StartUI(input, tracker, output).init();
-        String rsl = String.format("%s%s - %s\r\n\r\n%s", menu, item1.getName(), item1.getId(), menu);
+        String rsl = String.format("%s%s - %s%s%s%s", menu, item1.getName(), item1.getId(), LN, LN, menu);
         assertThat(rsl, is(new String(this.out.toByteArray())));
     }
 
@@ -70,7 +66,7 @@ public class StartUITest {
         Item item1 = tracker.add(new Item("test1", 123));
         Input input = new StubInput(new String[] {"2", item1.getId(), "test2", "6"});
         new StartUI(input, tracker, output).init();
-        String rsl = String.format("%sEditing successfully done - test2 Id - %s\r\n\r\n%s", menu, item1.getId(), menu);
+        String rsl = String.format("%sEditing successfully done - test2 Id - %s%s%s%s", menu, item1.getId(), LN, LN, menu);
         assertThat(rsl, is(new String(this.out.toByteArray())));
     }
 
@@ -80,7 +76,7 @@ public class StartUITest {
         String itemId = item1.getId();
         Input input = new StubInput(new String[] {"3", itemId, "6"});
         new StartUI(input, tracker, output).init();
-        String rsl = String.format("%sItem deleted successfully\r\nNow item with Id %s not exist\r\n\r\n%s", menu, item1.getId(), menu);
+        String rsl = String.format("%sItem deleted successfully%sNow item with Id %s not exist%s%s%s", menu, LN, item1.getId(), LN, LN, menu);
         assertThat(rsl, is(new String(this.out.toByteArray())));
     }
 
@@ -90,7 +86,7 @@ public class StartUITest {
         String itemId = item1.getId();
         Input input = new StubInput(new String[] {"4", itemId, "6"});
         new StartUI(input, tracker, output).init();
-        String rsl = String.format("%stest1 is required element.\r\n\r\n%s", menu, menu);
+        String rsl = String.format("%stest1 is required element.%s%s%s", menu, LN, LN, menu);
         assertThat(rsl, is(new String(this.out.toByteArray())));
     }
 
@@ -99,7 +95,7 @@ public class StartUITest {
         Item item1 = tracker.add(new Item("test1", 123));
         Input input = new StubInput(new String[] {"5", item1.getName(), "6"});
         new StartUI(input, tracker, output).init();
-        String rsl = String.format("%stest1 - %s\r\n\r\n%s", menu, item1.getId(), menu);
+        String rsl = String.format("%stest1 - %s%s%s%s", menu, item1.getId(), LN, LN, menu);
         assertThat(rsl, is(new String(this.out.toByteArray())));
     }
 }
